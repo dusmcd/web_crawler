1b78a573b2222f4024fa74df75edfa8d477c641f
Dustin McDowell <dustinmcdowell1989@gmail.com>
2024-06-06 10:57:41 -0400

    parsing html strings to get anchor tags

--- crawl.js
+++ crawl.js
@@ -1,14 +1,31 @@
-export const normalizeUrl = url => {
+import { JSDOM } from "jsdom"
+
+export function normalizeUrl(url) {
     const urlObj = new URL(url);
     let normalizedUrl = "";
     if (urlObj.username) {
-            normalizedUrl += `${urlObj.username}:${urlObj.password}@`
+            normalizedUrl += `${urlObj.username}:${urlObj.password}@`;
     }
     normalizedUrl += `${urlObj.host}${urlObj.pathname}`;
     let lastChar = normalizedUrl.length - 1;
     normalizedUrl = normalizedUrl[lastChar] === "/" ? normalizedUrl.slice(0, lastChar) : normalizedUrl;
-    normalizedUrl += `${urlObj.search}${urlObj.hash}`
+    normalizedUrl += `${urlObj.search}${urlObj.hash}`;
     lastChar = normalizedUrl.length - 1;
     normalizedUrl = normalizedUrl[lastChar] === "/" ? normalizedUrl.slice(0, lastChar) : normalizedUrl;
     return normalizedUrl;
+}
+
+export function getURLsFromHTML(html, baseURL) {
+    const dom = new JSDOM(html);
+    const links = dom.window.document.querySelectorAll("a");
+    const absoluteUrls = []
+    links.forEach(link => {
+        if (URL.canParse(link.href)) {
+            absoluteUrls.push(link.href);
+        } else {
+            absoluteUrls.push(`${baseURL}${link.href}`)
+        }
+    });
+    console.log(absoluteUrls);
+    return absoluteUrls;
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,29 +1,58 @@
 import { test, expect } from "@jest/globals";
-import { normalizeUrl } from "./crawl";
+import { normalizeUrl, getURLsFromHTML } from "./crawl";
 
-test("normalizes url strings", () => {
+test("normalizeUrl function", () => {
     expect(normalizeUrl("http://google.com/")).toBe("google.com");
     expect(normalizeUrl("https://www.google.com/search")).toBe("www.google.com/search");
-});
 
-test("parses username and password properly", () => {
     expect(normalizeUrl("https://username:password@example.com/")).toBe("username:password@example.com");
     expect(normalizeUrl("https://username:password@example.com/somepath/")).toBe("username:password@example.com/somepath")
-})
 
-test("test query string", () => {
     expect(normalizeUrl("http://example.com?search=something")).toBe("example.com?search=something");
     expect(normalizeUrl("http://example.com/listings?search=my_listing")).toBe("example.com/listings?search=my_listing");
-})
 
-test("test ports", () => {
     expect(normalizeUrl("http://example.com:74/home")).toBe("example.com:74/home");
     expect(normalizeUrl("https://example.com:55/search?page=here/")).toBe("example.com:55/search?page=here");
-})
 
-test("hashes", () => {
     expect(normalizeUrl("https://example.com/home#main")).toBe("example.com/home#main");
     expect(normalizeUrl("https://example.com/home#main/")).toBe("example.com/home#main");
     expect(normalizeUrl("http://example.com/search?page=here#main/")).toBe("example.com/search?page=here#main")
     expect(normalizeUrl("http://example.com/search?page=here#main")).toBe("example.com/search?page=here#main")
-})
+});
+
+test("getURLsFromHTML function", () => {
+    let expectedValue = ["https://example.com/link"];
+    let actualValue = getURLsFromHTML("<!DOCTYPE html><html><body><p>This is a <a href='/link'>Link</a></p></body></html>", "https://example.com");
+    for (let i = 0; i < expectedValue.length; i++) {
+        expect(expectedValue[i]).toBe(actualValue[i]);
+    }
+    let html = `
+        <html>
+            <body>
+                <p>
+                    Here is a <a href='/link'>Link</a>
+                    Here is some more text
+                </p>
+                <ul>
+                    <li>An absolute <a href='https://www.google.com'>link</a></li>
+                    <li>Another relative <a href='/something'>link</a></li>
+                    <li>Boring text</li>
+                </ul>
+                <p>
+                    A bunch of text. Lorem ipsum
+                    Some more really interesting text except this one has a link: <a href='/coolpage'>Cool page</a>
+                </p>
+            </body>
+        </html>
+    `
+    expectedValue = [
+        "http://bootdev.io/link",
+        "https://www.google.com/",
+        "http://bootdev.io/something",
+        "http://bootdev.io/coolpage"
+    ];
+    actualValue = getURLsFromHTML(html, "http://bootdev.io");
+    for (let i = 0; i < expectedValue.length; i++) {
+        expect(expectedValue[i]).toBe(actualValue[i])
+    }
+});
